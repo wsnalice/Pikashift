86ea9c2c6b41671fe06e436c9b4722c036acc332
Bristy Sikder <bristy@mit.edu>
2017-04-17 23:33:59 -0400

    Some more Message Links

--- MessageTab.js
+++ MessageTab.js
@@ -43,12 +43,29 @@ $(document).on('click', "#new-msg-btn", function(evt){
 
 $(document).on('click', "#new-msg-cancel-btn", function(evt){
   console.log('Cancel Clicked');
-  //window.location.href = "./LayoutPage.html";
+  window.location.href = "./LayoutPage.html";
 
 });
 
+$(document).ready(function()
+{
+   
+
+});
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
 $(document).on('click', "#new-msg-send-btn", function(evt){
   console.log('Send Clicked');
-  //window.location.href = "./LayoutPage.html";
-
+  // (TODO ) : Send confirmation Message. 
+  /*
+  var successString = '<div class="alert alert-success alert-dismissable col-md-10 fade in" id="new-msg-success"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a><strong>Success!</strong> Your Message has been sent.</div>'
+  var successAlert = $.parseHTML(successString);
+  await sleep(700);
+  */
+  window.location.href = "./LayoutPage.html";
+  
 });
