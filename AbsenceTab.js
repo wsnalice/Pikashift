b1fb408eacc3d1f90dc57bdb4777ea4bbf59cf13
Siqi Chen <Siqi Chen>
2017-04-18 11:59:02 -0400

    TabsIncluded

---
+++ AbsenceTab.js
@@ -0,0 +1,7 @@
+$(document).on('click', "#absence-cancel-btn", function(evt){
+  window.location.href = "./LayoutPage.html";
+});
+
+$(document).on('click', "#absence-submit-btn", function(evt){
+  window.location.href = "./LayoutPage.html";
+});
